cc4d40d3f51101074a650224e26185f4efd6de08
Hoseinparyab <hoseinparyab1@gmail.com>
2024-08-18 00:12:21 +0330

    CreatePageCategory-vol2

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,31 @@ use App\Http\Controllers\Admin\Market\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
-| ADmin Route
+| Web Routes
 |--------------------------------------------------------------------------
-
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::prefix("admin")->namespace("Admin")->group(function () {
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
 
     Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
 
-    Route::prefix('Market')->namepase("Admin")->group(function () {
-        //Category
+    Route::prefix('market')->namespace('Market')->group(function () {
+
+        //category
         Route::prefix('category')->group(function () {
             Route::get('/', [CategoryController::class, 'index'])->name('admin.market.category.index');
             Route::get('/create', [CategoryController::class, 'create'])->name('admin.market.category.create');
-            Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
             Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
             Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('admin.market.category.edit');
             Route::put('/update/{id}', [CategoryController::class, 'edit'])->name('admin.market.category.update');
