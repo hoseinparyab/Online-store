3ba88fb6a62be32a1fdb608bc7f05e1612df32d0
Hoseinparyab <hoseinparyab1@gmail.com>
2024-08-20 00:16:00 +0330

    fainal design market section to admin panel

--- app/Http/Controllers/Admin/Market/ProductController.php
+++ app/Http/Controllers/Admin/Market/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.market.product.create");
     }
 
     /**
--- resources/views/admin/market/product/create.blade.php
+++ resources/views/admin/market/product/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-<title>برند</title>
+<title>ایجاد کالا</title>
 @endsection
 
 @section('content')
@@ -10,8 +10,8 @@
     <ol class="breadcrumb">
       <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
       <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#">برند</a></li>
-      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد برند</li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">کالا </a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد کالا</li>
     </ol>
   </nav>
 
@@ -21,12 +21,12 @@
         <section class="main-body-container">
             <section class="main-body-container-header">
                 <h5>
-                  ایجاد برند
+                  ایجاد کالا
                 </h5>
             </section>
 
             <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{ route('admin.market.brand.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                <a href="{{ route('admin.market.product.index') }}" class="btn btn-info btn-sm">بازگشت</a>
             </section>
 
             <section>
@@ -35,17 +35,79 @@
 
                         <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">نام برند</label>
+                                <label for="">نام کالا</label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                         </section>
 
                         <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">دسته والد</label>
+                                <label for="">دسته کالا</label>
+                                <select name="" id="" class="form-control form-control-sm">
+                                    <option value="">دسته را انتخاب کنید</option>
+                                    <option value="">وسایل الکترونیکی</option>
+                                </select>
+                            </div>
+                        </section>
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">فرم کالا</label>
+                                <select name="" id="" class="form-control form-control-sm">
+                                    <option value="">دسته را انتخاب کنید</option>
+                                    <option value="">وسایل الکترونیکی</option>
+                                </select>
+                            </div>
+                        </section>
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">تصویر </label>
                                 <input type="file" class="form-control form-control-sm">
                             </div>
                         </section>
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">وزن</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                        </section>
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">قیمت کالا</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                        </section>
+                        <section class="col-12">
+                            <div class="form-group">
+                                <label for="">توضیحات</label>
+                                <textarea name="body" id="body"  class="form-control form-control-sm" rows="6"></textarea>
+                            </div>
+                        </section>
+
+                        <section class="col-12 border-top border-bottom py-3 mb-3">
+
+                            <section class="row">
+
+                                <section class="col-6 col-md-3">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-sm" placeholder="ویژگی ...">
+                                    </div>
+                                </section>
+
+                                <section class="col-6 col-md-3">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-sm" placeholder="مقدار ...">
+                                    </div>
+                                </section>
+
+                            </section>
+
+                            <section>
+                                <button type="button" class="btn btn-success btn-sm">افزودن</button>
+                            </section>
+
+
+                        </section>
+
                         <section class="col-12">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
@@ -56,5 +118,14 @@
         </section>
     </section>
 </section>
+
+@endsection
+
+@section('script')
+
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('body');
+    </script>
 
 @endsection
