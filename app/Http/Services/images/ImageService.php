47f77412e537c765c7c3b4bb12c90e0ea85253b8
Hoseinparyab <hoseinparyab1@gmail.com>
2024-09-03 20:11:30 +0330

    UploadServicesImage-part2

---
+++ app/Http/Services/images/ImageService.php
@@ -0,0 +1,12 @@
+<?php
+ namespace App\Http\Services\Images;
+
+use App\Http\Services\Image\ImageToolsService;
+
+ class ImageService extends ImageToolsService
+ {
+    public function save($image)
+    {
+        
+    }
+ }
