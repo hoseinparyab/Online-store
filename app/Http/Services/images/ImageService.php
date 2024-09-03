29dc43c44604186dc997cf2c8700ff18a3be9193
Hoseinparyab <hoseinparyab1@gmail.com>
2024-09-03 20:49:22 +0330

    UploadSevicesImage-part3

--- app/Http/Services/images/ImageService.php
+++ app/Http/Services/images/ImageService.php
@@ -1,12 +1,33 @@
 <?php
- namespace App\Http\Services\Images;
 
+namespace App\Http\Services\Images;
+
+
+use Intervention\Image\Facades\Image;
 use App\Http\Services\Image\ImageToolsService;
 
- class ImageService extends ImageToolsService
- {
+class ImageService extends ImageToolsService
+{
     public function save($image)
     {
-        
+        // set image
+        $this->setImage($image);
+        // execute provider
+        $this->provider();
+        // save image
+        $result = Image::make($image->getRealPath())->save(public_path($this->getImageAddress()), 60, $this->getImageFormat());
+        return $result ? $this->getImageAddress() : false;
+
     }
- }
+
+    public function fitAndSave($image, $width, $height)
+    {
+        //set image
+        $this->setImage($image);
+        //execute provider
+        $this->provider();
+        //save image
+        $result = Image::make($image->getRealPath())->fit($width, $height)->save(public_path($this->getImageAddress()), 60, $this->getImageFormat());
+        return $result ? $this->getImageAddress() : false;
+    }
+}
