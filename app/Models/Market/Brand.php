74ae974c706c826dda6708543ceb9664892ae400
Hoseinparyab <hoseinparyab1@gmail.com>
2024-09-30 22:06:43 +0330

    Creating product brands section in the admin panel

--- app/Models/Market/Brand.php
+++ app/Models/Market/Brand.php
@@ -2,10 +2,28 @@
 
 namespace App\Models\Market;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brand extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'original_name'
+            ]
+        ];
+    }
+
+    protected $casts = ['logo' => 'array'];
+
+
+    protected $fillable = ['persian_name', 'original_name', 'slug', 'logo', 'status', 'tags'];
+
+
 }
--- database/migrations/2024_08_22_174324_create_brands_table.php
+++ database/migrations/2024_08_22_174324_create_brands_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('perisan_name');
+            $table->string('persian_name');
             $table->string('original_name');
             $table->string('slug')->unique()->nullable();
             $table->text('logo');
