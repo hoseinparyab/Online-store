5fca78ad60a32796ae8f792644d500e6ec56eee5
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-12 18:01:05 +0330

    Detection of entering mobile number or email in login and registration

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth\Customer;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\Customer\LoginRegisterRequest;
@@ -14,7 +15,33 @@ class LoginRegisterController extends Controller
     }
     public function LoginRegister(LoginRegisterRequest $request)
     {
-        dd($request -> all());
-    }
+        $inputs = $request->all();
+        // check id  email or not
+        if (filter_var($inputs['id'], FILTER_VALIDATE_EMAIL)) {
 
+            $type = 1;  //=>email
+            $user = User::where('email', $inputs['id']->first());
+            if (empty($user)) {
+                $newUser['email'] = $inputs['id'];
+            }
+        }
+        //check id is mobile or not
+        elseif (preg_match('/^(\+98|98|0)9\d{9}$', $inputs['id'])) {
+            $type = 0; // 0 => mobile;
+
+
+            // all mobile numbers are in on format 9** *** ***
+            $inputs['id'] = ltrim($inputs['id'], '0');
+            $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'], 2) : $inputs['id'];
+            $inputs['id'] = str_replace('+98', '', $inputs['id']);
+
+            $user = User::where('mobile', $inputs['id'])->first();
+            if (empty($user)) {
+                $newUser['mobile'] = $inputs['id'];
+            }
+        } else {
+            $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
+            return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
+        }
+    }
 }
