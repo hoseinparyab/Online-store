490e583a27f0a316639375cf14bccaa47e70c133
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-15 16:45:42 +0330

    Confirm the activation code

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Otp extends Model
 {
     use HasFactory;
+
     protected $guarded = ['id'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
