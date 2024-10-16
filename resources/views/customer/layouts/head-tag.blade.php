2af2e4863fee12cac24d6507f387d62ea862f7ae
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-16 22:19:41 +0330

    Construction of banners section(Debug)

--- resources/views/customer/layouts/head-tag.blade.php
+++ resources/views/customer/layouts/head-tag.blade.php
@@ -6,8 +6,11 @@
 <link rel="stylesheet" href="{{ asset('customer-assets/css/bootstrap/bootstrap-reboot.rtl.min.css') }}">
 <link rel="stylesheet" href="{{ asset('customer-assets/css/bootstrap/bootstrap.rtl.min.css') }}">
 <link rel="stylesheet" href="{{ asset('customer-assets/fontawesome/css/all.min.css') }}">
-<link rel="stylesheet" href="{{ asset('customer-assets/plugins/owlcarousel/assets/owl.carousel.min.css') }}">
-<link rel="stylesheet" href="{{ asset('customer-assets/plugins/owlcarousel/assets/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
 <link rel="stylesheet" href="{{ asset('customer-assets/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('customer-assets/css/cart.css') }}">
 <link rel="stylesheet" href="{{ asset('customer-assets/css/address.css') }}">
