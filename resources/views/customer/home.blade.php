73fc395ad5b5edc82a6d61ff790a515626a7b466
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-19 13:55:47 +0330

    Internal page of the product (reading information from the database)

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -66,7 +66,7 @@
                                         <section class="product">
                                             {{-- <section class="product-add-to-cart"><a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="افزودن به سبد خرید"><i class="fa fa-cart-plus"></i></a></section> --}}
                                             {{-- <section class="product-add-to-favorite"><a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="افزودن به علاقه مندی"><i class="fa fa-heart"></i></a></section> --}}
-                                            <a class="product-link" href="#">
+                                            <a class="product-link" href="{{ route('customer.market.product',$mostVisitedProduct ) }}">
                                                 <section class="product-image">
                                                     <img class="" src="{{ asset($mostVisitedProduct->image['indexArray']['medium']) }}" alt="{{ $mostVisitedProduct->name }}">
                                                 </section>
@@ -149,7 +149,7 @@
                                         <section class="product">
                                             {{-- <section class="product-add-to-cart"><a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="افزودن به سبد خرید"><i class="fa fa-cart-plus"></i></a></section> --}}
                                             {{-- <section class="product-add-to-favorite"><a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="افزودن به علاقه مندی"><i class="fa fa-heart"></i></a></section> --}}
-                                            <a class="product-link" href="#">
+                                            <a class="product-link" href="{{ route('customer.market.product',$offerProduct ) }}">
                                                 <section class="product-image">
                                                     <img class="" src="{{ asset($offerProduct->image['indexArray']['medium']) }}" alt="{{ $offerProduct->name }}">
                                                 </section>
