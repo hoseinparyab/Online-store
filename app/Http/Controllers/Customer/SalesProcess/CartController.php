45b9949dac0f92020fea42876556ff2472c5ec19
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-22 20:08:54 +0330

    Add to cart (part one)

---
+++ app/Http/Controllers/Customer/SalesProcess/CartController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Customer\SalesProcess;
+
+use Illuminate\Http\Request;
+use App\Models\Market\Product;
+use App\Models\Market\CartItem;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function cart() {}
+
+    public function updateCart() {}
+
+
+    public function addToCart(Product $product, Request $request)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'color' => 'nullable|exists:product_colors,id',
+                'guarantee' => 'nullable|exists:guarantees,id',
+                'number' => 'numeric|min:1|max:5'
+            ]);
+
+            $cartItems = CartItem::where('product_id', $product->id)->where('user_id', auth()->user()->id)->get();
+
+            if (!isset($request->color)) {
+                $request->color = null;
+            }
+            if (!isset($request->guarantee)) {
+                $request->guarantee = null;
+            }
+
+            foreach ($cartItems as $cartItem) {
+                if ($cartItem->color_id == $request->color && $cartItem->guarantee_id == $request->guarantee) {
+                    if ($cartItem->number != $request->number) {
+                        $cartItem->update(['number' => $request->number]);
+                    }
+                    return back();
+                }
+            }
+
+            $inputs = [];
+            $inputs['color_id'] = $request->color;
+            $inputs['guarantee_id'] = $request->guarantee;
+            $inputs['user_id'] =  auth()->user()->id;
+            $inputs['product_id'] =  $product->id;
+
+            CartItem::create($inputs);
+
+            return back();
+        } else {
+            return redirect()->route('auth.customer.login-register-form');
+        }
+    }
+
+
+    public function removeFromCart() {}
+}
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Http\Controllers\Admin\Notify\EmailFileController;
 use App\Http\Controllers\Admin\Ticket\TicketAdminController;
 use App\Http\Controllers\Admin\Market\ProductColorController;
 use App\Http\Controllers\Admin\Market\PropertyValueController;
+use App\Http\Controllers\Customer\SalesProcess\CartController;
 use App\Http\Controllers\Admin\Ticket\TicketCategoryController;
 use App\Http\Controllers\Admin\Ticket\TicketPriorityController;
 use App\Http\Controllers\Auth\Customer\LoginRegisterController;
@@ -417,12 +418,26 @@ Route::namespace('Auth')->group(function () {
 
 Route::get('/', [HomeController::class, 'home'])->name('customer.home');
 
+
+Route::namespace('SalesProcess')->group(function () {
+
+    Route::get('/cart', [CartController::class, 'cart'])->name('customer.sales-process.cart');
+    Route::post('/cart', [CartController::class, 'updatecart'])->name('customer.sales-process.update-cart');
+    Route::post('/add-to-cart{product:slug}', [CartController::class, 'addToCart'])->name('customer.sales-process.add-to-cart');
+    Route::post('/remove-form-cart{cartItem}', [CartController::class, 'removeFromCart'])->name('customer.sales-process.remove-from-cart');
+
+});
+
+
 Route::namespace('Market')->group(function () {
 
     Route::get('/product/{product:slug}', [MarketProductController::class, 'product'])->name('customer.market.product');
     Route::post('/add-comment/prodcut/{product:slug}', [MarketProductController::class, 'addComment'])->name('customer.market.add-comment');
     Route::get('/add-to-favorite/prodcut/{product:slug}', [MarketProductController::class, 'addToFavorite'])->name('customer.market.add-to-favorite');
 });
+
+
+
 
 Route::middleware([
     'auth:sanctum',
