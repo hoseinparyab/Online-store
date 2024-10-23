20fe02dd3f33c348528d99b3067d4d027b0ee43b
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-23 21:31:59 +0330

    Remove the item from the shopping cart

--- routes/web.php
+++ routes/web.php
@@ -422,9 +422,10 @@ Route::get('/', [HomeController::class, 'home'])->name('customer.home');
 Route::namespace('SalesProcess')->group(function () {
 
     Route::get('/cart', [CartController::class, 'cart'])->name('customer.sales-process.cart');
-    Route::post('/cart', [CartController::class, 'updatecart'])->name('customer.sales-process.update-cart');
-    Route::post('/add-to-cart{product:slug}', [CartController::class, 'addToCart'])->name('customer.sales-process.add-to-cart');
-    Route::post('/remove-form-cart{cartItem}', [CartController::class, 'removeFromCart'])->name('customer.sales-process.remove-from-cart');
+    Route::post('/cart', [CartController::class, 'updateCart'])->name('customer.sales-process.update-cart');
+    Route::post('/add-to-cart/{product:slug}', [CartController::class, 'addToCart'])->name('customer.sales-process.add-to-cart');
+    Route::get('/remove-from-cart/{cartItem}', [CartController::class, 'removeFromCart'])->name('customer.sales-process.remove-from-cart');
+
 
 });
 
