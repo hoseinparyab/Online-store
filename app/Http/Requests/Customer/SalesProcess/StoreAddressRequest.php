8cc9259c97f5c027a0bcf57dbcf1542b2cdb1685
Hoseinparyab <hoseinparyab1@gmail.com>
2024-10-28 18:47:44 +0330

    Complete address editing

--- app/Http/Requests/Customer/SalesProcess/StoreAddressRequest.php
+++ app/Http/Requests/Customer/SalesProcess/StoreAddressRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Customer\SalesProcess;
 
+use App\Rules\PostalCode;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreAddressRequest extends FormRequest
@@ -13,7 +14,7 @@ class StoreAddressRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,23 @@ class StoreAddressRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'province_id' => 'required|exists:provinces,id',
+            'city_id' => 'required|exists:cities,id',
+            'address' => 'required|min:1|max:300',
+            'postal_code' => ['required', new PostalCode()],
+            'no' => 'required',
+            'unit' => 'required',
+            'receiver' => 'sometimes',
+            'recipient_first_name' => 'required_with:receiver,on',
+            'recipient_last_name' => 'required_with:receiver,on',
+            'mobile' => 'required_with:receiver,on',
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'unit' => 'واحد',
+            'mobile' => 'موبایل گیرنده',
         ];
     }
 }
